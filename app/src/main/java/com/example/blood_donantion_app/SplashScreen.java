445500ed2313d99f49c13d147dbf860cc2d80e0d
SynthiaAtieno <ondengsynthia222@gmail.com>
2022-08-02 21:09:16 +0300

    Created the splash screen for the application

--- app/src/main/java/com/example/blood_donantion_app/SplashScreen.java
+++ app/src/main/java/com/example/blood_donantion_app/SplashScreen.java
@@ -3,12 +3,32 @@ package com.example.blood_donantion_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+    ImageView logo;
+    TextView title, slogan;
+    Animation top_anim, bottom_anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        slogan = findViewById(R.id.slogan);
+
+        top_anim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottom_anim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo.setAnimation(top_anim);
+        title.setAnimation(bottom_anim);
+        slogan.setAnimation(bottom_anim);
+
+
     }
 }
