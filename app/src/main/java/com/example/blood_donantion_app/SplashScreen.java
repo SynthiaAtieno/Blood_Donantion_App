114dac4af9697e330133d991a7d7736936eb0f4c
SynthiaAtieno <ondengsynthia222@gmail.com>
2022-08-02 21:20:05 +0300

    Added full Screen Flag and Login Activity

--- app/src/main/java/com/example/blood_donantion_app/SplashScreen.java
+++ app/src/main/java/com/example/blood_donantion_app/SplashScreen.java
@@ -2,7 +2,10 @@ package com.example.blood_donantion_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -16,6 +19,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splashscreen);
 
         logo = findViewById(R.id.logo);
@@ -29,6 +33,15 @@ public class SplashScreen extends AppCompatActivity {
         title.setAnimation(bottom_anim);
         slogan.setAnimation(bottom_anim);
 
+        int Splash_screen = 4300;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, Splash_screen);
 
     }
 }
