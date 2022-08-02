5e3ab486741e877d070c266587f7493b98812a53
SynthiaAtieno <ondengsynthia222@gmail.com>
2022-08-02 23:05:18 +0300

    Added Register activity and some functionalities

---
+++ app/src/main/java/com/example/blood_donantion_app/DonorRegistration.java
@@ -0,0 +1,14 @@
+package com.example.blood_donantion_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonorRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_registration);
+    }
+}
---
+++ app/src/main/java/com/example/blood_donantion_app/Select_Registration.java
@@ -0,0 +1,14 @@
+package com.example.blood_donantion_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Select_Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_registration);
+    }
+}
