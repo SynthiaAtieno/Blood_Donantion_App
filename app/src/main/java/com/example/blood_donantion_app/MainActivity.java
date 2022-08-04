c0cae688915bea3360f2d781425a61e258cbbad7
SynthiaAtieno <ondengsynthia222@gmail.com>
2022-08-04 18:26:39 +0300

    added the user_layout.xml and modified the main activity

--- app/src/main/java/com/example/blood_donantion_app/MainActivity.java
+++ app/src/main/java/com/example/blood_donantion_app/MainActivity.java
@@ -7,22 +7,32 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.blood_donantion_app.adapter.UserAdapter;
+import com.example.blood_donantion_app.model.Users;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -34,6 +44,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private TextView nav_fullname, nav_email, nav_type, nav_bloodgroup;
     private CircleImageView profile;
     DatabaseReference userRef;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+    private List<Users> usersList;
+    private UserAdapter userAdapter;
 
 
     @Override
@@ -44,6 +58,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout = findViewById(R.id.drwerLayout);
         toolbar = findViewById(R.id.toolbca);
         navigationView = findViewById(R.id.nav_view);
+        recyclerView = findViewById(R.id.recyclerview);
+        progressBar = findViewById(R.id.progressbar);
+
+        usersList =new ArrayList<>();
+        userAdapter = new UserAdapter(MainActivity.this,usersList);
+        recyclerView.setAdapter(userAdapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users")
+                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String type = snapshot.child("type").getValue().toString();
+
+                if (type.equals("Donor"))
+                {
+                    readRecipient();
+                }
+                else
+                {
+                    readDonors();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         navigationView.setNavigationItemSelectedListener(this);
 
         nav_fullname = navigationView.getHeaderView(0).findViewById(R.id.nav_userFullname);
@@ -52,6 +96,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         profile = navigationView.getHeaderView(0).findViewById(R.id.nav_user_image);
         nav_bloodgroup = navigationView.getHeaderView(0).findViewById(R.id.nav_userBloodgroup);
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
         userRef = FirebaseDatabase.getInstance().getReference().child("users").child(
                 FirebaseAuth.getInstance().getCurrentUser().getUid());
         userRef.addValueEventListener(new ValueEventListener() {
@@ -98,6 +147,64 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
 
+    }
+
+    private void readDonors() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users");
+        Query query = reference.orderByChild("type").equalTo("Donor");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usersList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren());
+                {
+                    Users user = snapshot.getValue(Users.class);
+                    usersList.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+
+                if (usersList.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "No Donors found", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void readRecipient() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users");
+        Query query = reference.orderByChild("type").equalTo("Recipient");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                usersList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren());
+                {
+                    Users user = snapshot.getValue(Users.class);
+                    usersList.add(user);
+                }
+                userAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+
+                if (usersList.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "No Donors found", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
